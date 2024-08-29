273b143ab751925045510a2b2e85507d15cef3df
PragatiDahal <pragatidahal2058@gmail.com>
2024-08-29 21:02:55 +0545

    update

---
+++ firstprogram.py
@@ -0,0 +1,18 @@
+print("Hello World",5,"Pragati") #printing hello world, 5 and pragati
+print(15)
+print(15*14)
+print(15/14)
+print(15-14)
+print(15+14)
+
+# This is a song lyrics
+# here \n is a escape character
+print("I am no beauty queen,i am just beautiful me. You got every right to a beautiful life,\n Who says? Who says you aren't perfect? Who says you aren't worth it? \n Who says you are the only one that's hurting? Trust me that's the price of beauty")
+
+
+print ("Hey I am a \"good girl.\"\n He is also a good boy") #here \ is escape character
+print('Hey I am\'a\"good girl.\"\n He is also a good boy') 
+
+# here sep specify how to seperate the objects and end specify what to print at the end
+print("Hey",3,5, sep="~",end="005\n")
+print("Pragati")
