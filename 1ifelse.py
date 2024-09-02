787f98df1550bbccecf6e05b84848af7923636a2
PragatiDahal <pragatidahal2058@gmail.com>
2024-09-02 21:32:55 +0545

    update

---
+++ 1ifelse.py
@@ -0,0 +1,21 @@
+a=int(input("Enter your age:"))
+print("Your age is",a)
+#Conditional operators
+# >,<,<=,>=,==,!=
+# print(a>18)
+# print(a<=18)
+# print(a==18)
+# print(a!=18)
+
+if(a>18):
+    print("You can drive")
+else:
+    print("You cannot drive")
+
+
+appleprice=210
+budget=200
+if(appleprice <= budget):
+    print("Alexa add 1 kg apples to cart")
+else:
+    print("Alexa, do not add 1 kg apples to cart")
---
+++ 2elif.py
@@ -0,0 +1,9 @@
+num=int(input("Enter a number:"))
+if (num<0):
+    print("The number is negative")
+elif (num==0):
+    print("The number is zero")
+else:
+    print("The number is positive")
+
+print("I am happy now.") #here i am happy now doesnot have any connection with if,elif and else statement
